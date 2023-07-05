e72619b2b3934e1b67f37fb86b94d7c4fe40fae6
parvin-noori <parvinnoori0@gmail.com>
2023-07-05 11:28:21 +0330

    insert newest

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,39 @@
 $(document).ready(function () {
 
 
-    new Swiper('.slider', {
+    new Swiper('.slider', {});
 
+    new Swiper(".newest", {
+        slidesPerView: 2,
+        slidesPerGroup:1,
+        spaceBetween: 10,
+        pagination: {
+            el: ".swiper-pagination-newest",
+            clickable: true,
+        },
+        navigation: {
+            nextEl: ".swiper-button-next-newest",
+            prevEl: ".swiper-button-prev-newest",
+        },
+        breakpoints: {
+            640: {
+                slidesPerView: 2,
+                slidesPerGroup:2,
+            },
+            768: {
+                slidesPerView: 3,
+                spaceBetween: 20,
+                slidesPerGroup:3,
+            },
+            992: {
+                slidesPerView: 3,
+                slidesPerGroup:3,
+            },
+            1024: {
+                slidesPerView: 5,
+                slidesPerGroup:5,
+            },
+        },
     });
-
 });
 
