23c4b275d31e4814761c44ba0e2f747464113919
parvin-noori <parvinnoori0@gmail.com>
2023-07-16 15:40:09 +0330

    fix logo on center in responsive

--- assets/js/main.js
+++ assets/js/main.js
@@ -127,5 +127,23 @@ $(document).ready(function () {
         e.preventDefault();
         $('.thumb-video').addClass('hide')
     })
+
+    //footer-menu in responsive(accordion)
+    function accordtionLinksItem() {
+        var acc = document.getElementsByClassName("accordion-button");
+        var i;
+
+        for (i = 0; i < acc.length; i++) {
+            acc[i].addEventListener("click", function () {
+                this.classList.toggle("active");
+                var panel = this.nextElementSibling;
+                if (panel.style.maxHeight) {
+                    panel.style.maxHeight = null;
+                } else {
+                    panel.style.maxHeight = panel.scrollHeight + "px";
+                }
+            });
+        }
+    }
 });
 
